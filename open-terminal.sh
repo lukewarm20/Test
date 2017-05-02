8e0eb54c90bbfe764a8517268f5af1e23b7b692a
lukewarm20 <lukewillb20@gmail.com>
2017-05-02 02:32:32 +0000

    Create open-terminal.sh

---
+++ open-terminal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xdotool key alt+q 
+#make sure you change the open command in gnome under settings-keyboard-system-then change "show the command prompt" funtion keys won't work in xdotool. Change the alt+f2 to alt+q
+xdotool type gnome-terminal
+xdotool key KP_Enter
